c31dc21fdf95e810e7809479152c2d873019a148
TaiYiLee <terry911613@gmail.com>
2019-05-15 23:52:04 +0800

    go go coding

---
+++ EzOrder(Res)/Controller/TableManage/TableManageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TableManageViewController.swift
+//  EzOrder(Res)
+//
+//  Created by 李泰儀 on 2019/5/15.
+//  Copyright © 2019 TerryLee. All rights reserved.
+//
+
+import UIKit
+
+class TableManageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
