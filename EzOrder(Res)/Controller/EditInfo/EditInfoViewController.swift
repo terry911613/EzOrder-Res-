21d2dab816a9dde0931382ca786de761cb6c6e5d
LiuShTing <s86842222@gmail.com>
2019-06-14 20:40:59 +0800

    拜託

--- EzOrder(Res)/Controller/EditInfo/EditInfoViewController.swift
+++ EzOrder(Res)/Controller/EditInfo/EditInfoViewController.swift
@@ -65,7 +65,7 @@ class EditInfoViewController: UIViewController,CLLocationManagerDelegate{
         print("suck it")
         
         if let resID = resID{
-            db.collection("res").document(resID).addSnapshotListener { (res, error) in
+    db.collection("res").document(resID).addSnapshotListener { (res, error) in
                 print("mother fucker")
                 if let resData = res?.data(){
                     print("sucker")
